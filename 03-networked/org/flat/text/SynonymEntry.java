91a8237dd91debc479d34f87ac8d4557763edb61
Marten Hernebring <marten.hernebring@gmail.com>
2021-03-10 09:26:36 +0100

    Created project

---
+++ 03-networked/org/flat/text/SynonymEntry.java
@@ -0,0 +1,31 @@
+package org.flat.text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SynonymEntry {
+  
+  private String word;
+  private List<String>synonyms;
+  
+  public SynonymEntry(String word) {
+    this.word = word;
+  }
+
+  public String word() {
+    return word;
+  }
+  
+  public int numberOfSynonyms() {
+    return synonyms.size();
+  }
+  
+  public List<String>synonyms() {
+    // First time, get synonyms and cache them
+    if (synonyms==null) {
+      synonyms = Synonyms.getSynonyms(word);
+    }
+    // Synonyms.getSynonyms is guaranteed not to return null
+    return synonyms;
+  }
+}
